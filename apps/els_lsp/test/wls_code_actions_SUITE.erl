20151cdba540384c8abe70a10f1b2c88af39c4ee
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2022-05-26 13:19:06 +0200

    tests

---
+++ apps/els_lsp/test/wls_code_actions_SUITE.erl
@@ -0,0 +1,346 @@
+-module(wls_code_actions_SUITE).
+
+%% CT Callbacks
+-export([ suite/0
+        , init_per_suite/1
+        , end_per_suite/1
+        , init_per_testcase/2
+        , end_per_testcase/2
+        , all/0
+        ]).
+
+%% Test cases
+-export([generalise_fun_expression/1]).
+-export([introduce_new_var/1]).
+-export([move_fun_fun_call/1]).
+-export([move_fun_fun_name/1]).
+-export([extract_fun_one_expr/1]).
+-export([extract_fun_more_expr/1]).
+-export([new_macro_expr/1]).
+-export([new_macro_pattern/1]).
+-export([rename_fun/1]).
+-export([rename_var/1]).
+-export([fold_expression/1]).
+
+%%==============================================================================
+%% Includes
+%%==============================================================================
+-include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+%%==============================================================================
+%% Types
+%%==============================================================================
+-type config() :: [{atom(), any()}].
+
+%%==============================================================================
+%% CT Callbacks
+%%==============================================================================
+-spec suite() -> [tuple()].
+suite() ->
+  [{timetrap, {seconds, 30}}].
+
+-spec all() -> [atom()].
+all() ->
+  els_test_utils:all(?MODULE).
+
+-spec init_per_suite(config()) -> config().
+init_per_suite(Config) ->
+  els_test_utils:init_per_suite(Config).
+
+-spec end_per_suite(config()) -> ok.
+end_per_suite(Config) ->
+  els_test_utils:end_per_suite(Config).
+
+-spec init_per_testcase(atom(), config()) -> config().
+init_per_testcase(TestCase, Config) ->
+  Config2 = els_test_utils:init_per_testcase(TestCase, Config),
+  els_config:set(wrangler, #{
+    "enabled" => true,
+    "enabled_refactorings" => [
+      "generalise-fun",
+      "fold-expression",
+      "generalise-fun",
+      "move-fun",
+      "new-fun",
+      "new-macro",
+      "new-var",
+      "rename-fun",
+      "rename-var"]
+  }),
+  Config2.
+
+-spec end_per_testcase(atom(), config()) -> ok.
+end_per_testcase(TestCase, Config) ->
+  els_test_utils:end_per_testcase(TestCase, Config).
+
+
+-spec generalise_fun_expression(config()) ->  ok.
+generalise_fun_expression(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {8, 13},
+                                to => {8, 15}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-generalise-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Generalise function">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"variable">>, 'text' => <<"New argument's name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Generalise function">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec introduce_new_var(config()) ->  ok.
+introduce_new_var(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {11, 3},
+                                to => {11, 15}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-new-var">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Introduce new variable">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"variable">>, 'text' => <<"Variable name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Introduce new variable">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec move_fun_fun_name(config()) ->  ok.
+move_fun_fun_name(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {10, 4},
+                                to => {10, 4}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-move-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Move function">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"file">>, 'text' => <<"File name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Move function">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec move_fun_fun_call(config()) ->  ok.
+move_fun_fun_call(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {13, 9},
+                               to => {13, 9}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-move-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+    <- Result, Command =:= PrefixedCommand],
+  Expected =
+  [ #{ command => #{
+    title => <<"Move function">>,
+    command => PrefixedCommand,
+    arguments => [#{
+      range => Range,
+      uri   => Uri,
+      user_input => #{'type' => <<"file">>, 'text' => <<"File name">>}
+  }]},
+      kind => <<"refactor">>,
+      title => <<"Wrangler: Move function">>
+    }
+  ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec extract_fun_one_expr(config()) ->  ok.
+extract_fun_one_expr(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {12, 14},
+                                to => {12, 23}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-new-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Extract function">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"atom">>, 'text' => <<"The new function`s name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Extract function">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+-spec extract_fun_more_expr(config()) ->  ok.
+extract_fun_more_expr(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {12, 3},
+                                to => {14, 43}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-new-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Extract function">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"atom">>, 'text' => <<"The new function`s name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Extract function">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec new_macro_expr(config()) ->  ok.
+new_macro_expr(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {23, 3},
+                                to => {23, 6}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-new-macro">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Introduce new macro">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"macro">>, 'text' => <<"Macro name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Introduce new macro">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec new_macro_pattern(config()) ->  ok.
+new_macro_pattern(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {36, 5},
+                                to => {36, 7}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-new-macro">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Introduce new macro">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"macro">>, 'text' => <<"Macro name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Introduce new macro">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+-spec rename_fun(config()) ->  ok.
+rename_fun(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {25, 3},
+                                to => {25, 3}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-rename-fun">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Rename Function">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"atom">>, 'text' => <<"New function name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Rename Function">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+rename_var(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {34, 6},
+                                to => {34, 6}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-rename-var">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Rename Variable">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri,
+       user_input => #{'type' => <<"variable">>, 'text' => <<"New variable name">>}
+   }]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Rename Variable">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
+
+fold_expression(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Range = els_protocol:range(#{from => {17, 2},
+                                to => {17, 2}}),
+  PrefixedCommand = els_command:with_prefix(<<"wrangler-fold-expression">>),
+  #{result := Result} = els_client:document_codeaction(Uri, Range, []),
+  FilteredResult = [L || #{ command := #{ command := Command }} = L
+                        <- Result, Command =:= PrefixedCommand],
+  Expected =
+    [ #{ command => #{
+     title => <<"Fold expression">>,
+     command => PrefixedCommand,
+     arguments => [#{
+       range => Range,
+       uri   => Uri}]},
+        kind => <<"refactor">>,
+        title => <<"Wrangler: Fold expression">>
+      }
+    ],
+  ?assertEqual(Expected, FilteredResult),
+  ok.
--- apps/els_lsp/test/wls_server_SUITE.erl
+++ apps/els_lsp/test/wls_server_SUITE.erl
@@ -1,18 +1,13 @@
 -module(wls_server_SUITE).
 
 %% CT Callbacks
--export([ suite/0
-        , init_per_suite/1
-        , end_per_suite/1
-        , init_per_testcase/2
-        , end_per_testcase/2
-        , all/0
-        ]).
-
+-export([suite/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
+         end_per_testcase/2, all/0]).
 %% Test cases
 -export([empty/1]).
 -export([start_refactoring/1]).
--export([refactor_a_candidate/1]).
+-export([refactor_candidate/1]).
+-export([exit_form/1]).
 
 %%==============================================================================
 %% Includes
@@ -47,30 +42,21 @@ end_per_suite(Config) ->
 -spec init_per_testcase(atom(), config()) -> config().
 init_per_testcase(TestCase, Config) ->
   Config2 = els_test_utils:init_per_testcase(TestCase, Config),
-  els_config:set(wrangler, #{
-    "enabled" => true,
-    "enabled_refactorings" => ["fold-expression"]
-  }),
+  els_config:set(wrangler,
+                 #{"enabled" => true, "enabled_refactorings" => ["fold-expression"]}),
   wls_server:start(),
+  Uri = ?config(wls_uri, Config2),
+  Path = wls_utils:path(Uri),
+  UriBackup = ?config(wls_backup_uri, Config2),
+  PathBackup = wls_utils:path(UriBackup),
+  file:copy(PathBackup, Path),
   Config2.
 
 -spec end_per_testcase(atom(), config()) -> ok.
 end_per_testcase(TestCase, Config) ->
-  els_test_utils:end_per_testcase(TestCase, Config),
-  % Delete header if it exists
-  Uri = ?config(wls_uri, Config),
-  Path = wls_utils:path(Uri),
-  {ok, Text} = file:read_file(Path),
-  case binary:split(Text, <<"\n">>) of
-    [<<"%! Wrangler refactor form.">>, RemText1] ->
-        [_, RemText2] = binary:split(RemText1, <<"\n">>),
-        [_, RemText3] = binary:split(RemText2, <<"\n">>),
-        file:write_file(Path, RemText3);
-    _ -> ok
-  end.
+  els_test_utils:end_per_testcase(TestCase, Config).
 
-
--spec empty(config()) ->  ok.
+-spec empty(config()) -> ok.
 empty(Config) ->
   Uri = ?config(wls_uri, Config),
   Path = wls_utils:path(Uri),
@@ -78,18 +64,68 @@ empty(Config) ->
   ?assertEqual(not_exists, Result),
   ok.
 
--spec start_refactoring(config()) ->  ok.
+-spec start_refactoring(config()) -> ok.
 start_refactoring(Config) ->
   Uri = ?config(wls_uri, Config),
   Path = wls_utils:path(Uri),
-  Result = wls_server:start_refactoring(Path, fold_expression, {17, 3}),
-  ?assertEqual(ok, Result),
+  % Check new state
+  ok = wls_server:start_refactoring(Path, fold_expression, {17, 3}),
   {ok, Candidates, Data} = wls_code_action_fold_expression:calculate_regions(Path, {20, 3}),
-  Expected = {under_refactoring,
-    #{refactor => fold_expression, regions => Candidates, data => Data}
-  },
+  Expected =
+    {under_refactoring,
+     #{refactor => fold_expression,
+       regions => Candidates,
+       data => Data}},
   ?assertEqual(Expected, wls_server:get_state(Path)),
+
+  % Check code lenses
+  PrefixedCommandExit = els_command:with_prefix(<<"wrangler-form-exit">>),
+  PrefixedCommandFold = els_command:with_prefix(<<"wrangler-fold-expression">>),
+  #{result := Result} = els_client:document_codelens(Uri),
+  FilteredResultExit =
+    [L || #{command := #{command := Command}} = L <- Result, Command =:= PrefixedCommandExit],
+  FilteredResultFold =
+    [L || #{command := #{command := Command}} = L <- Result, Command =:= PrefixedCommandFold],
+  ExpectedExit =
+    [#{command =>
+         #{arguments => [#{uri => Uri}],
+           command => PrefixedCommandExit,
+           title => <<"Exit">>},
+       data => [],
+       range =>
+         #{'end' => #{character => EC, line => EL}, start => #{character => SC, line => SL}}}
+       || {EC, EL, SC, SL} <- [{40,29,2,29}, {40,33,2,33}, {1, 0, 0, 0}]],
+  ?assertEqual(ExpectedExit, FilteredResultExit),
+  ExpectedFold =
+    [#{command =>
+         #{arguments => [#{uri => Uri, index => Index}],
+           command => PrefixedCommandFold,
+           title => <<"Refactor this instance">>},
+       data => [],
+       range =>
+         #{'end' => #{character => EC, line => EL}, start => #{character => SC, line => SL}}}
+       || {EC, EL, SC, SL, Index} <- [{40,29,2,29,1}, {40,33,2,33,2}]],
+  ?assertEqual(ExpectedFold, FilteredResultFold),
   ok.
 
--spec refactor_a_candidate(config()) ->  ok.
-refactor_a_candidate(_Config) -> ok.
+
+-spec refactor_candidate(config()) -> ok.
+refactor_candidate(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Path = wls_utils:path(Uri),
+  wls_server:start_refactoring(Path, fold_expression, {18, 3}),
+  {under_refactoring, #{regions := Regions}} = wls_server:get_state(Path),
+  ?assertEqual(2, length(Regions)),
+  wls_code_action_fold_expression:execute_command([#{ <<"uri">> => Uri, <<"index">> => 1}]),
+  {under_refactoring, #{regions := Regions2}} = wls_server:get_state(Path),
+  ?assertEqual(1, length(Regions2)),
+  wls_code_action_fold_expression:execute_command([#{ <<"uri">> => Uri, <<"index">> => 1}]),
+  ?assertEqual(not_exists, wls_server:get_state(Path)).
+
+-spec exit_form(config()) -> ok.
+exit_form(Config) ->
+  Uri = ?config(wls_uri, Config),
+  Path = wls_utils:path(Uri),
+  wls_server:start_refactoring(Path, fold_expression, {18, 3}),
+  wls_server:exit_form(Path),
+  ?assertEqual(not_exists, wls_server:get_state(Path)).
